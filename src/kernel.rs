a0fe4a4e4224d704184e327126d2064d0e4e726d
Abe Kohandel <abe.kohandel@gmail.com>
2023-04-16 12:11:09 -0700

    feat: add kernel driver and module information
    Add the ability to dump kernel driver and module information when using
    verbose output.
    
    $ cargo run --bin lspci -- -vs00:14.3
        Finished dev [unoptimized + debuginfo] target(s) in 0.02s
         Running `target/debug/lspci '-vs00:14.3'`
    00:14.3 Network controller: Intel Corporation Alder Lake-P PCH CNVi WiFi (rev 01)
            Subsystem: Intel Corporation Wi-Fi 6 AX201 160MHz
            Kernel driver in use: iwlwifi
            Kernel modules: iwlwifi

---
+++ src/kernel.rs
@@ -0,0 +1,59 @@
+use crate::bdf::BusDeviceFunction;
+use crate::error::Result;
+use crate::sysfs::Sysfs;
+use std::fs::read_link;
+
+#[derive(Debug)]
+pub struct Kernel;
+
+impl Kernel {
+    pub fn text(&self, bdf: &BusDeviceFunction, verbosity: u8) -> Result<String> {
+        Ok(format!(
+            "{}\n{}",
+            self.driver_text(bdf, verbosity)?,
+            self.module_text(bdf, verbosity)?
+        ))
+    }
+
+    pub fn driver_text(&self, bdf: &BusDeviceFunction, verbosity: u8) -> Result<String> {
+        if verbosity == 0 {
+            return Ok(String::new());
+        }
+
+        let driver_symlink = Sysfs::get_function_sub_path(bdf, "driver");
+        let driver_path = read_link(driver_symlink);
+
+        if driver_path.is_err() {
+            return Ok(String::new());
+        }
+
+        let driver_path = driver_path?;
+        let driver_path = driver_path.to_str().unwrap_or_default();
+
+        Ok(format!(
+            "\tKernel driver in use: {}",
+            driver_path.split('/').last().unwrap_or_default()
+        ))
+    }
+
+    pub fn module_text(&self, bdf: &BusDeviceFunction, verbosity: u8) -> Result<String> {
+        if verbosity == 0 {
+            return Ok(String::new());
+        }
+
+        let module_symlink = Sysfs::get_function_sub_path(bdf, "driver/module");
+        let module_path = read_link(module_symlink);
+
+        if module_path.is_err() {
+            return Ok(String::new());
+        }
+
+        let module_path = module_path?;
+        let module_path = module_path.to_str().unwrap_or_default();
+
+        Ok(format!(
+            "\tKernel modules: {}",
+            module_path.split('/').last().unwrap_or_default()
+        ))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod bdf;
 pub mod caps;
 pub mod error;
 pub mod function;
+pub mod kernel;
 pub mod parser;
 pub mod sysfs;
 pub mod vdc;
